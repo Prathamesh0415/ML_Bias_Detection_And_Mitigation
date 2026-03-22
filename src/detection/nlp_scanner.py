aa99ecaf7fdf71db71258c0f207ec53895d6687c
Prathamesh Ghormare <prathameshghormare@gmail.com>
2026-03-22 21:56:12 +0530

    add zero shot classification pipeline

---
+++ src/detection/nlp_scanner.py
@@ -0,0 +1,30 @@
+from transformers import pipeline
+import logging
+
+logger = logging(__name__)
+
+class ZeroShotScanner:
+    def __init__(self, model_name, candidate_labels, threshold):
+        logging.info(f"initializing nlp scanner Model: {model_name}")
+        self.classifier = pipeline('zero-shot-classification', model=model_name)
+        self.candidate_labels = candidate_labels
+        self.threshold = threshold
+
+    def scan_columns(self, columns: list) -> list:
+        """ Function to identify the columns which might induce bias using ZeroShotClassification """
+        flaged_columns = []
+        logger.info("Starting zero shot classification to identify columns that might induce bias")
+
+        for col in columns:
+            sequence_to_classify = f"This columns contains data about persons {col.replace(" ", "_")}"
+            res = self.classifier(sequence_to_classify, self.candidate_labels)
+            top_label = res['label'][0]
+            top_score = res['score'][0]
+
+            if top_label != "financial transaction" and top_score >= self.threshold:
+                logger.warning(f"flagged column: {col} with confidence score: {top_score}")
+                flaged_columns.append(col)
+            else:
+                logger.info(f"Passed: {col}")
+        
+        return flaged_columns
